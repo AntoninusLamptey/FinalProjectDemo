2abe42d0084b3a1c63df6b0ff38814dbc8bb3c17
AntoninusLamptey <antoninus@mail.usf.edu>
2017-04-13 16:26:04 -0400

    dafsd

--- FinalProjectDemo/myTableViewController.m
+++ FinalProjectDemo/myTableViewController.m
@@ -7,11 +7,14 @@
 //
 
 #import "myTableViewController.h"
+#import <dispatch/dispatch.h>
+
 
 @interface myTableViewController (){
     NSMutableArray *myArray;
     NSDictionary *myDict;
 }
+
 
 @end
 
@@ -26,12 +29,10 @@
     //Assign the data from the URL
     NSData *myData = [NSData dataWithContentsOfURL:myUrl];
     
-    //JSON Parsing implementation
-    NSDictionary *myDict = [NSJSONSerialization JSONObjectWithData:myData options:NSJSONReadingMutableContainers error:nil];
-    NSDictionary *mrData = [myDict objectForKey:@"MRData"];
-    //fetching array inside
-    NSArray *races = [[mrData objectForKey:@"RaceTable"] objectForKey:@"Races"];
     
+    //Parsing JSON and adding data to relevant array
+    myDict = [NSJSONSerialization JSONObjectWithData:myData options:NSJSONReadingMutableContainers error:nil];
+    NSDictionary *races = [[[myDict objectForKey:@"MRData"] objectForKey:@"RaceTable"] objectForKey:@"Races"];
     for(NSDictionary *tempValue in races){
         NSError *error;
         if(!error)
@@ -40,10 +41,11 @@
         [myArray addObject:[[tempValue [@"Circuit"] objectForKey:@"Location"] objectForKey:@"locality"]];
         
         }
-        
+        [self.tableView reloadData];
         
         
     }
+    
 
     //self..text = [myArray componentsJoinedByString:@"\n"];
     NSLog(@"%@", [myArray componentsJoinedByString:@"\n"]);
@@ -59,53 +61,11 @@
         cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
     }
    //cell.textLabel.text = [myArray objectAtIndex:indexPath.row];
-    cell.textLabel.text =@"Hello";
+    cell.textLabel.text = [myArray objectAtIndex:indexPath.row];
     return cell;
 }
 
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
